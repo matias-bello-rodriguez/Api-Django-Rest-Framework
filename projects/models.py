654447cf754fe590c696959af42abeb779500c56
matilinucs <materodriguez200011@gmail.com>
2023-05-12 19:23:38 -0400

    render 2.0

--- projects/models.py
+++ projects/models.py
@@ -2,11 +2,9 @@ from django.db import models
 
 # Create your models here.
 class Project(models.Model):
-    nombre = models.CharField(max_length=200)
-    descripcion = models.TextField()
-    categoria = models.CharField(max_length=50)
-    precio = models.IntegerField()
-    stock = models.IntegerField()
-    created_at =  models.DateTimeField(auto_now_add=True)
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    technology = models.CharField(max_length=200)
+    created_at = models.DateTimeField(auto_now_add=True)
 
                 
