fdc534e070aec47a72178e9e39d729eef62730e4
matilinucs <materodriguez200011@gmail.com>
2023-05-12 21:18:23 -0400

    xico

--- projects/models.py
+++ projects/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 class Project(models.Model):
-    title = models.CharField(max_length=200)
+    nombre = models.CharField(max_length=200)
     description = models.TextField()
     technology = models.CharField(max_length=200)
     created_at = models.DateTimeField(auto_now_add=True)
