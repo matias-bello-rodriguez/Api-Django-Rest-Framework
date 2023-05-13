d972b35c3a00954fbe71ab6b0cc40bd92002945f
matilinucs <materodriguez200011@gmail.com>
2023-05-12 21:13:39 -0400

    surrealista

--- projects/migrations/0001_initial.py
+++ projects/migrations/0001_initial.py
@@ -16,7 +16,6 @@ class Migration(migrations.Migration):
             fields=[
                 ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
                 ('title', models.CharField(max_length=200)),
-                ('nombre', models.CharField(max_length=200)),
                 ('description', models.TextField()),
                 ('technology', models.CharField(max_length=200)),
                 ('created_at', models.DateTimeField(auto_now_add=True)),
